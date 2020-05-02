ac025aa1c6194d95877546c278be7a08cc02363f
Boubacar DIENE <boubacar.diene@gmail.com>
2020-05-02 18:59:33 +0200

    [DEV] Add pre-commit hook to run clang-format/tidy

---
+++ cmake/git/pre-commit-hook.cmake
@@ -0,0 +1,29 @@
+##
+#
+# \file pre-commit-hook.cmake
+#
+# \author Boubacar DIENE <boubacar.diene@gmail.com>
+# \date   April 2020
+#
+# \brief  CMake script to copy pre-commit-hook.in template to git
+#         hooks directory as pre-commit bash script and replace
+#         CMake's variable inside with their value
+#
+##
+
+#################################################################
+#                          Installation                         #
+#################################################################
+
+if(NOT EXISTS ${CMAKE_SOURCE_DIR}/.git/hooks)
+    message(WARNING "Not a valid git project")
+else()
+    if(EXISTS ${CMAKE_SOURCE_DIR}/.git/hooks/pre-commit)
+        message(VERBOSE "pre-commit script already installed")
+    else()
+        configure_file(
+            ${CMAKE_SOURCE_DIR}/cmake/git/pre-commit-hook.in
+            ${CMAKE_SOURCE_DIR}/.git/hooks/pre-commit @ONLY
+        )
+    endif()
+endif()
