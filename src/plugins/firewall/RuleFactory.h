7c2cc19d5e1b186adec8912d81edce93ffdafd88
Boubacar DIENE <boubacar.diene@gmail.com>
2020-05-04 06:17:18 +0200

    [DEV] Add copyright banner to source files

--- src/plugins/firewall/RuleFactory.h
+++ src/plugins/firewall/RuleFactory.h
@@ -1,3 +1,26 @@
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
+//                                                                          //
+//  Copyright © 2020 Boubacar DIENE                                         //
+//                                                                          //
+//  This file is part of NetworkService project.                            //
+//                                                                          //
+//  NetworkService is free software: you can redistribute it and/or modify  //
+//  it under the terms of the GNU General Public License as published by    //
+//  the Free Software Foundation, either version 2 of the License, or       //
+//  (at your option) any later version.                                     //
+//                                                                          //
+//  NetworkService is distributed in the hope that it will be useful,       //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of          //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           //
+//  GNU General Public License for more details.                            //
+//                                                                          //
+//  You should have received a copy of the GNU General Public License       //
+//  along with NetworkService. If not, see <http://www.gnu.org/licenses/>   //
+//  or write to the Free Software Foundation, Inc., 51 Franklin Street,     //
+//  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           //
+//                                                                          //
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
+
 #ifndef __PLUGINS_FIREWALL_RULE_FACTORY_H__
 #define __PLUGINS_FIREWALL_RULE_FACTORY_H__
 
